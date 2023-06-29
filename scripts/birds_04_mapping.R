bc6f5ceb13b9e7d642f01058f8f93fd9b24ccb67
JDuarteNiemen <rvy20jmu@uea.ac.uk>
2023-06-29 22:26:01 +0000

    error producing correct points from UMT Coords, most likely an error in the spatial object.

--- scripts/birds_04_mapping.R
+++ scripts/birds_04_mapping.R
@@ -3,8 +3,9 @@
 #Packages ----
 library(tidyverse)
 library(highcharter)
-
-
+library(sp)
+library(rgdal)
+### Map visualisation ----
 world_map <- map_data("world")
 
 world_map %>%
@@ -43,3 +44,117 @@ hc <- highchart() %>%
     name = "LifeExpectancy"
   ) 
  hc
+ 
+### Reading coordinates ----
+
+#converting MGRS to lat/long
+ 
+ site_code <- c("CAWW",
+               "PANC",
+               "AMAZ",
+               "OSOB",
+               "SWAM",
+               "EUGE",
+               "LEON",
+               "HOBO",
+               "NAVI",
+               "LAGU",
+               "ISLA",
+               "NEFA",
+               "NEF2",
+               "RIO2",
+               "TARI")
+ 
+ UTM_zone <- c("19L",
+           "19L",
+           "19L",
+           "19L",
+           "19L",
+           "19L",
+           "19L",
+           "19L",
+           "19L",
+           "19L",
+           "19L",
+           "19L",
+           "19L",
+           "19L",
+           "19L")
+ 
+ UTM_easting <- c("502115",
+                  "502207",
+                  "502653",
+                  "501682",
+                  "501760",
+                  "501283",
+                  "501690",
+                  "502081",
+                  "500095",
+                  "500782",
+                  "501060",
+                  "501343",
+                  "503359",
+                  "502591",
+                  "502174")
+ 
+ UTM_northing <- c("8615299",
+                   "8615188",
+                   "8615287",
+                   "8615023",
+                   "8614654",
+                   "8614050",
+                   "8613747",
+                   "8615455",
+                   "8615332",
+                   "8615897",
+                   "8616083",
+                   "8616064",
+                   "8615456",
+                   "8615959",
+                   "8616098")
+ 
+ #Create data frame with site name and UTM coords
+UTM_data <- data.frame(site_code, UTM_zone, UTM_easting, UTM_northing)
+
+#Convert to numeric
+UTM_data$UTM_easting <-  as.numeric(UTM_data$UTM_easting)
+UTM_data$UTM_northing <-  as.numeric(UTM_data$UTM_northing)
+ 
+#Create a spatial object using UTM coords
+data_sp <- SpatialPoints(UTM_data[, c("UTM_easting", "UTM_northing")], proj4string = CRS("+proj=utm +zone=19 +datum=WGS84 +units=m +no_defs"))
+
+ 
+#Convert spatial object to lat/long
+data_ll <- spTransform(data_sp, CRS("+proj=longlat +datum=WGS84"))
+
+
+lat_long <- coordinates(data_ll)
+
+ 
+###Plotting site location on map ----
+
+
+# Convert the matrix to a data frame
+my_data <- as.data.frame(lat_long)
+
+# Use fortify with the converted data frame
+UTM_plot_data <- fortify(my_data)
+
+#add group variable
+UTM_plot_data$group <- 1124
+
+
+#Get map
+world_map <- map_data("world")
+
+world_map %>%
+  ggplot(aes(x = long,
+             y = lat,
+             group = group))+
+  geom_path()+
+  geom_point(data = UTM_plot_data, aes(x = UTM_easting,
+                                        y = UTM_northing),
+             colour= "red") +
+  xlim(-82,-68)+
+  ylim(-18.5,0)+
+  theme_minimal()
